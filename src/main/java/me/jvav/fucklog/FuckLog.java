f5d69fad0b0fb34c722e5c2a270d7a0fc5f2aef3
HuajiMUR233 <13060847725@163.com>
2022-01-19 15:51:19 +0800

    2.0.0 :(

--- src/main/java/me/jvav/fucklog/FuckLog.java
+++ src/main/java/me/jvav/fucklog/FuckLog.java
@@ -2,13 +2,12 @@ package me.jvav.fucklog;
 
 import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.Logger;
 
 public class FuckLog implements ModInitializer {
-    static final String MOD_VERSION = "1.0.0";
+    static final String MOD_VERSION = "2.0.0";
     @Override
     public void onInitialize() {
-        Logger LOGGER = LogManager.getLogger("FuckLog");
-        LOGGER.info("FuckLog v{} by Huaji_MUR233 loaded.", MOD_VERSION);
+        ((Logger) LogManager.getRootLogger()).addFilter(new LogFilter());
     }
 }
